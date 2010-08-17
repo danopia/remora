ffd03223afbce5e708553d6cf995c296b4aa5b5f
Nathan B <nathan.f77@gmail.com>
2010-08-17 10:52:36 +0800

    Added 'check_dependencies' script, to check for bundler gem and mplayer package.

---
+++ check_dependencies.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+
+puts "=== Checking that your system has the required packages and gems installed..."
+
+# Check that bundler is installed
+begin
+  require 'bundler'
+  puts "===== Bundler is installed."
+rescue Exception => e
+  puts "==!!! Please install gem 'bundler'   ====> $ sudo gem install bundler"
+end
+
+# Check that mplayer is installed
+apt_search = `aptitude search mplayer`
+if apt_search[/^(i [A ] mplayer)/, 1]
+  puts "===== mplayer is installed."
+else
+  puts "==!!! Please install package 'mplayer'   ====> $ sudo apt-get install mplayer"
+end
+
